0e9d55e9d191820708e3acbd7ab98d09017a8f50
bogstandard <eric.daddio@gmail.com>
2021-08-08 17:33:49 +0100

    Fix: Remove unneeded dep

--- src/main/java/com/rlweather/RlweatherPlugin.java
+++ src/main/java/com/rlweather/RlweatherPlugin.java
@@ -3,7 +3,6 @@ package com.rlweather;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
